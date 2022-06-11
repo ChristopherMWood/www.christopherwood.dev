e80203647121b40375e88229c71c9de6418e3a8e
Christopher Wood <cmw2379@gmail.com>
2022-06-11 00:45:37 -0400

    basic splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to ChristopherWood.dev test
+          Welcome to ChristopherWood.dev
         </p>
+        <div>
         <a
           className="App-link"
           href="https://github.com/christopherMWood"
@@ -18,6 +17,24 @@ function App() {
         >
           Github
         </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/cmw2379/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>
+
+        <a
+          className="App-link"
+          href="https://exercism.org/profiles/ChristopherMWood"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Exercism
+        </a>
+        </div>
       </header>
     </div>
   );
