6c7ca1d4963c3525acacb17560f204e6f54ac67b
Christopher Wood <cmw2379@gmail.com>
2022-06-21 00:28:36 -0400

    Fixed warnings

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -7,13 +7,13 @@ function GlobalNav() {
   const [hideHome, setHideHome] = React.useState(true);
 
   React.useEffect(() => {
-    setHideHome(location.pathname == '/');
+    setHideHome(location.pathname === '/');
   }, [location])
   
   return (
       <nav className="App-header">
         <Link hidden={hideHome} to="/" id="home-link" className="btn draw-border" >HOME</Link>
-        <a className="btn draw-border">CONTACT</a>
+        <button className="btn draw-border">CONTACT</button>
         <Link to="/api" className="btn draw-border" >API</Link>
         <Link to="/projects" className="btn draw-border" >PROJECTS</Link>
         <Link to="/about" className="btn draw-border" >ABOUT</Link>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,37 +1,10 @@
-import React from 'react';
 import './styles.scss'
 import githubLogo from '../../images/GitHub-Mark-Light-120px-plus.png';
 import linkedinLogo from '../../images/linkedin-light-logo.png';
 import exercismLogo from '../../images/exercism-purple-logo.png';
-import Typed from 'typed.js';
-import { Link } from "react-router-dom";
+
 
 function Home() {
-	const el = React.useRef(null);
-	const el2 = React.useRef(null);
-	const typed = React.useRef(null);
-	const typed2 = React.useRef(null);
-
-  // React.useEffect(() => {
-  //   const options = {
-  //   	strings: [
-  //       'backend developer at heart',
-  //       'full stack developer by trade'
-  //     ],
-  //     typeSpeed: 50,
-  //   };
-    
-  //   // elRef refers to the <span> rendered below
-  //   typed.current = new Typed(el.current, options);
-  //   typed2.current = new Typed(el2.current, options);
-    
-  //   return () => {
-  //     // Make sure to destroy Typed instance during cleanup
-  //     // to prevent memory leaks
-  //     typed.current.destroy();
-  //   }
-  // }, [])
-
   return (
     <div>
       <div id="page-content">
